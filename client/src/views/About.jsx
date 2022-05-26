f574f81d670a209a9c3e284ada606f8082cfb5ce
LoganBErspamer <104615504+LoganBErspamer@users.noreply.github.com>
2022-05-26 16:08:57 -0700

    Update About.jsx

---
+++ client/src/views/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+        <h1>About Opus</h1>
+
+        <h3>Opus is the cummulative work of creators Logan Erspamer and Lexington Carey in an effort to create a multifunctional user orietnated web page. Opus is an eccomerace and socal media platform wrapped into one. This is in an effort to provide ease of use and convience to the guests. The users can shop around for books and also make posts that can range from things such as review, thoughts, and wishlists of books. This allows our users to be able to get in depth reviews about books they are curious about written by other user and also helps users to find their next interesting read. </h3>
+
+        <p>This project is still in its alpha release. Stay tuned for more information of what is to come</p>
+
+        <h1>In the Works</h1>
+        <h3>Integrating a login and registration to allow users to create accounts and follow eachother's feed</h3>
+    </div>
+  )
+}
+
+export default about
