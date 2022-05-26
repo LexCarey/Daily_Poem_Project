78681af585ac2c06a0fe964ba0fca6311b40269b
LoganBErspamer <104615504+LoganBErspamer@users.noreply.github.com>
2022-05-26 16:12:12 -0700

    Update Contact.jsx

---
+++ client/src/views/Contact.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>
+        <h1>Contacts</h1>
+        <p>Feel free to contact us at these emails to leave suggestions and ideas for the website as well as any bugs</p>
+
+        <h1>Senior Devs</h1>
+        <p>Loganerspamer@gmail.com</p>
+        <p>lexcarey77@gmail.com</p>
+
+
+        <h1>Purchase Support</h1>
+        <p>For infromation reguarding refunds, lost packages and purchase queries, contact us at:</p>
+        <p>OpusSupport@gmail.com</p>
+    </div>
+  )
+}
+
+export default contact
